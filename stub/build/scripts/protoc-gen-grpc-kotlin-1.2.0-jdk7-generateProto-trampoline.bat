bc1633ca5f2cf2526a785f9f5fb9e2cbb2f192a4
mordehaibenharush <63501119+mordehaibenharush@users.noreply.github.com>
2021-12-26 20:50:08 +0200

    Added support for multiple proto files

--- stub/build/scripts/protoc-gen-grpc-kotlin-1.2.0-jdk7-generateProto-trampoline.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-"C:\Users\DELL-PC\.jdks\corretto-16.0.2\bin\java.exe" -jar "C:\Users\DELL-PC\.gradle\caches\modules-2\files-2.1\io.grpc\protoc-gen-grpc-kotlin\1.2.0\c82a225d1263ce1fc304792af10225e96a4f1e5a\protoc-gen-grpc-kotlin-1.2.0-jdk7.jar" %*
